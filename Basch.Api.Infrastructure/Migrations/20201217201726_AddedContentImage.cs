84d1817bf98d543ea97365ec43c49d9ae1c81476
jackfperryjr <jackfperryjr@gmail.com>
2020-12-17 15:18:31 -0500

    Added new column.

---
+++ Basch.Api.Infrastructure/Migrations/20201217201726_AddedContentImage.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Basch.Api.Infrastructure.Migrations
+{
+    public partial class AddedContentImage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ContentImage",
+                table: "Announcements",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ContentImage",
+                table: "Announcements");
+        }
+    }
+}
